dd7a11d30298d0245dfec49bbe6c0920a166ba1d
estela.romero.edu <romeroestela3@gmail.com>
2024-03-20 17:00:07 +0100

    Agregando React y Bootstap

---
+++ OneDrive/Escritorio/PortfolioEstela/portfolioEstela/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+<script src='./node_modelues/bootstrap/dist/js/bootstrap.grid.css'></script>
+
+function App() {
+  return (
+    <>
+     <nav className="navbar bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand">Navbar</a>
+    <form className="d-flex" role="search">
+      <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+      <button className="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+
+Hola mi nombre es Estela
+    </>
+  )
+}
+
+export default App
